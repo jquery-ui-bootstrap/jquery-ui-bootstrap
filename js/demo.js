2f5c592222a240794a80dee22555fdc71f9d5dad
gerits aurelien <gerits.aurelien@gmail.com>
2012-10-16 23:20:11 +0200

    * Add ui spinner in css and javascript demo

--- js/demo.js
+++ js/demo.js
@@ -111,6 +111,31 @@ $(function () {
     //Menu
     $("#menu").menu();
 
+    var spinner = $( "#spinner" ).spinner();
+
+    $( "#disable" ).click(function() {
+        if ( spinner.spinner( "option", "disabled" ) ) {
+            spinner.spinner( "enable" );
+        } else {
+            spinner.spinner( "disable" );
+        }
+    });
+    $( "#destroy" ).click(function() {
+        if ( spinner.data( "ui-spinner" ) ) {
+            spinner.spinner( "destroy" );
+        } else {
+            spinner.spinner();
+        }
+    });
+    $( "#getvalue" ).click(function() {
+        alert( spinner.spinner( "value" ) );
+    });
+    $( "#setvalue" ).click(function() {
+        spinner.spinner( "value", 5 );
+    });
+
+    $( "button" ).button();
+
     // Split button
     $("#rerun").button().click(function () {
         alert("Running the last action");
