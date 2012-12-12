77d9931a79dbae46fce84a13849b2d4b2b8cb929
Gerits aurelien <gerits.aurelien@gmail.com>
2012-12-12 13:02:26 +0100

    * fix bug Tabs with close icon and clean demo js
    * fix bug Tabs with close icon
    * Clean demo.js
    * Add open dialog in combination examples (tabs)

--- js/demo.js
+++ js/demo.js
@@ -6,7 +6,7 @@ $(function () {
     });
 
     // Tabs
-    $('#tabs2, #tabs').tabs();
+    $('#tabs2, #tabs, #tabs3').tabs();
 
     // Buttons
     $('button').button();
@@ -154,136 +154,89 @@ $(function () {
     }).parent().buttonset();
 
 
-    var $tab_title_input = $("#tab_title"),
-        $tab_content_input = $("#tab_content");
-    var tab_counter = 2;
-
-    // tabs init with a custom tab template and an "add" callback filling in the content
-    var $tabs = $("#tabs2").tabs({
-        tabTemplate: "<li><a href='#{href}'>#{label}</a></li>",
-        add: function (event, ui) {
-            var tab_content = $tab_content_input.val() || "Tab " + tab_counter + " content.";
-            $(ui.panel).append("<p>" + tab_content + "</p>");
-        }
-    });
-
-    // modal dialog init: custom buttons and a "close" callback reseting the form inside
-    var $dialog = $("#dialog2").dialog({
-        autoOpen: false,
-        modal: true,
-        buttons: {
-            Add: function () {
-                addTab();
-                $(this).dialog("close");
-            },
-            Cancel: function () {
-                $(this).dialog("close");
-            }
-        },
-        open: function () {
-            $tab_title_input.focus();
-        },
-        close: function () {
-            $form[0].reset();
-        }
-    });
-
-    // addTab form: calls addTab function on submit and closes the dialog
-    var $form = $("form", $dialog).submit(function () {
-        addTab();
-        $dialog.dialog("close");
-        return false;
-    });
-
-    // actual addTab function: adds new tab using the title input from the form above
-
-
-    function addTab() {
-        var tab_title = $tab_title_input.val() || "Tab " + tab_counter;
-        $tabs.tabs("add", "#tabs-" + tab_counter, tab_title);
-        tab_counter++;
-    }
-
-    // addTab button: just opens the dialog
-    $("#add_tab").button().click(function () {
-        $dialog.dialog("open");
-    });
-
-    // close icon: removing the tab on click
-    // note: closable tabs gonna be an option in the future - see http://dev.jqueryui.com/ticket/3924
-    $("#tabs span.ui-icon-close").on("click", function () {
-        var index = $("li", $tabs).index($(this).parent());
-        $tabs.tabs("remove", index);
-    });
-
     // Filament datepicker
     $('#rangeA').daterangepicker();
     $('#rangeBa, #rangeBb').daterangepicker();
 
 
     // Dynamic tabs
-    var $tab_title_input = $("#tab_title"),
-        $tab_content_input = $("#tab_content");
-    var tab_counter = 2;
-
-    // tabs init with a custom tab template and an "add" callback filling in the content
-    var $tabs = $("#tabs2").tabs({
-        tabTemplate: "<li><a href='#{href}'>#{label}</a></li>",
-        add: function (event, ui) {
-            var tab_content = $tab_content_input.val() || "Tab " + tab_counter + " content.";
-            $(ui.panel).append("<p>" + tab_content + "</p>");
-        }
-    });
+    var tabTitle = $( "#tab_title" ),
+            tabContent = $( "#tab_content" ),
+            tabTemplate = "<li><a href='#{href}'>#{label}</a> <span class='ui-icon ui-icon-close'>Remove Tab</span></li>",
+            tabCounter = 2;
+ 
+    var tabs = $( "#tabs2" ).tabs();
 
     // modal dialog init: custom buttons and a "close" callback reseting the form inside
-    var $dialog = $("#dialog2").dialog({
+    var dialog = $( "#dialog2" ).dialog({
         autoOpen: false,
         modal: true,
         buttons: {
-            Add: function () {
+            Add: function() {
                 addTab();
-                $(this).dialog("close");
+                $( this ).dialog( "close" );
             },
-            Cancel: function () {
-                $(this).dialog("close");
+            Cancel: function() {
+                $( this ).dialog( "close" );
             }
         },
-        open: function () {
-            $tab_title_input.focus();
-        },
-        close: function () {
-            $form[0].reset();
+        close: function() {
+            form[ 0 ].reset();
         }
     });
 
     // addTab form: calls addTab function on submit and closes the dialog
-    var $form = $("form", $dialog).submit(function () {
+    var form = dialog.find( "form" ).submit(function( event ) {
         addTab();
-        $dialog.dialog("close");
-        return false;
+        dialog.dialog( "close" );
+        event.preventDefault();
     });
 
-    // actual addTab function: adds new tab using the title input from the form above
-
+    // actual addTab function: adds new tab using the input from the form above
     function addTab() {
-        var tab_title = $tab_title_input.val() || "Tab " + tab_counter;
-        $tabs.tabs("add", "#tabs-" + tab_counter, tab_title);
-        tab_counter++;
+        var label = tabTitle.val() || "Tab " + tabCounter,
+            id = "tabs-" + tabCounter,
+            li = $( tabTemplate.replace( /#\{href\}/g, "#" + id ).replace( /#\{label\}/g, label ) ),
+            tabContentHtml = tabContent.val() || "Tab " + tabCounter + " content.";
+
+        tabs.find( ".ui-tabs-nav" ).append( li );
+        tabs.append( "<div id='" + id + "'><p>" + tabContentHtml + "</p></div>" );
+        tabs.tabs( "refresh" );
+        tabCounter++;
     }
 
     // addTab button: just opens the dialog
-    $("#add_tab").button().click(function () {
-        $dialog.dialog("open");
-    });
+    $( "#add_tab" )
+        .button()
+        .click(function() {
+            dialog.dialog( "open" );
+        });
 
     // close icon: removing the tab on click
-    // note: closable tabs gonna be an option in the future - see http://dev.jqueryui.com/ticket/3924
-    $("#tabs span.ui-icon-close").on("click", function () {
-        var index = $("li", $tabs).index($(this).parent());
-        $tabs.tabs("remove", index);
+    $( "#tabs2" ).on( "click",'span.ui-icon-close', function() {
+        
+        var panelId = $( this ).closest( "li" ).remove().attr( "aria-controls" );
+        $( "#" + panelId ).remove();
+        tabs.tabs( "refresh" );
     });
 
-
+    //Combination examples (tabs) and open dialog
+    $('#sampleButton').on('click', function(event){
+        event.preventDefault();
+        $('#dialog').dialog({
+            autoOpen: true,
+            modal: true,
+            width: 600,
+            buttons: {
+                "Save": function () {
+                    $(this).dialog("close");
+                },
+                "Cancel": function () {
+                    $(this).dialog("close");
+                }
+            }
+        });
+    });
     // File input (using http://filamentgroup.com/lab/jquery_custom_file_input_book_designing_with_progressive_enhancement/)
     $('#file').customFileInput({
         button_position : 'right'
