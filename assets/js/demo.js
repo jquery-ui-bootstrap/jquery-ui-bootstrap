9d549eb81ad0e41b5ebef26b274fbc72a715c8b0
Gerits aurelien <gerits.aurelien@gmail.com>
2013-01-30 15:59:55 +0100

    * Fix bug Button with icon position (#117)
    * Add Demo split button

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -40,6 +40,35 @@ $(function () {
     $("#shuffle").button();
     $("#repeat").buttonset();
 
+    // Split button
+
+    $( "#rerun" )
+    .button()
+    .click(function() {
+        alert( "Running the last action" );
+    })
+    .next()
+    .button({
+        text: false,
+        icons: {
+            primary: "ui-icon-triangle-1-s"
+        }
+    })
+    .click(function() {
+        var menu = $( this ).parent().next().show().position({
+            my: "left top",
+            at: "left bottom",
+            of: this
+        });
+        $( document ).one( "click", function() {
+            menu.hide();
+        });
+        return false;
+    }).parent()
+    .buttonset()
+    .next()
+    .hide()
+    .menu();
     //####### Accordion
     $("#menu-collapse").accordion({
         header: "h3"
